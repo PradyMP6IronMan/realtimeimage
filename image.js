96606a1b7567829556239473aa8d42e40d554275
PradyMP6IronMan <71083246+PradyMP6IronMan@users.noreply.github.com>
2020-09-28 19:50:24 +0530

    Add files via upload

---
+++ image.js
@@ -0,0 +1,26 @@
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.position(475, 350);
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/l9U0ayFSn/model.json', modelLoaded);
+}
+
+function modelLoaded() {
+    console.log("Model has Loaded");
+}
+
+function draw() {
+    image(video, 0, 0, 300, 350);
+    classifier.classify(video, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
